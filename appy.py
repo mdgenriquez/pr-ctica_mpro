df315ae66b52aa9863044531e4d668fe8ae6a5ba
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-03-07 17:03:10 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+from stmol import showmol
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
+from rdkit.Chem import Descriptors
+from padelpy import from_smiles
+# from PaDEL_pywrapper import PaDEL
+# from PaDEL_pywrapper import descriptors
+import numpy as np
+import pickle
+import joblib
 
+st.title("Predictor de docking score de ligando-receptor Mpro")
